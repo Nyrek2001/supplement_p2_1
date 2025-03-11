2186d7e30b810e4364848f14cdcf92d9f79da4dc
NyrekWheeler <142206465+Nyrek2001@users.noreply.github.com>
2025-03-11 17:02:11 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,31 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+/// # Description
+/// This function takes a length and width as a paremeter and returns the area of a rectangle
+/// # Paremeters
+/// - length the length of the rectangle
+/// - width the width of the rectangle
+/// the area of the rectangle with the specified dimensions.
+pub fn get_rectangle_area(length: f64, width: f64) -> f64{
+   
+    if length == width{
+       return length*length 
     }
+    length*width   }
+/// # Description
+/// This function takes a radius as a paremeter and returns the area of a circle
+/// # Paremeters
+/// - radius the radius of the circle
+/// the area of the circle with the specified dimensions.
+
+pub fn get_circle_area(radius: f64) -> f64{
+    std::f64::consts::PI * f64::powi(radius, 2)
 }
+/// # Description
+/// This function takes a base and height as a paremeter and returns the area of a triangle
+/// # Paremeters
+/// - base the base of the triangle
+/// - height the height of the triangle
+/// the area of the triangle with the specified dimensions.
+pub fn get_triangle_area(base: f64, heigth: f64) -> f64{
+    base * height * 0.5
+}
+ mod test:
